1791009a6d447a300d76554b9af507117ab38958
root <root@DESKTOP-L6G6VAR.>
2025-05-18 17:17:40 -0300

    Adding CORS middleware

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"knapsack-problem/controllers"
+  "github.com/gin-contrib/cors"
 )
 
 func main() {
   router := gin.Default()
+  config := cors.DefaultConfig()
+  config.AllowOrigins = []string{"http://localhost:5173"}
+
+  router.Use(cors.New(config))
   api := router.Group("/api")
   {
 	api.POST("/solve", controllers.SolveKnapsackProblemController)
